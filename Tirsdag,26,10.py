563867270098bd5c3c44ab2e90fb0935f7a13150
Tobias Engli Fladen <tobiae1805@osloskolen.no>
2021-10-26 09:46:12 +0200

    ja

--- Tirsdag,26,10.py
+++ Tirsdag,26,10.py
@@ -1,4 +1,5 @@
-BensinTank = input("Skriv inn størrelsen på tanken: ")
-BensinForbruk = input("Skriv inn bensin forbruk pr mil: ")
-Pris = input("Skriv inn bensin prisen pr liter: ")
-Lengde = input("Skriv inn hvor du skal kjøre: ")
+BensinTank = float(input("Skriv inn størrelsen på tanken: "))
+BensinForbruk = float(input("Skriv inn bensin forbruk pr mil: "))
+Pris = float(input("Skriv inn bensin prisen pr liter: "))
+Lengde = float(input("Skriv inn hvor du skal kjøre: "))
+
