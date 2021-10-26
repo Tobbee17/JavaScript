0759cb24649d73d665988f0721e4146173b75a82
Tobias Engli Fladen <tobiae1805@osloskolen.no>
2021-10-26 09:30:53 +0200

    ja

---
+++ Tirsdag,26,10.py
@@ -0,0 +1,4 @@
+BensinTank = input("Skriv inn størrelsen på tanken: ")
+BensinForbruk = input("Skriv inn bensin forbruk pr mil: ")
+Pris = input("Skriv inn bensin prisen pr liter: ")
+Lengde = input("Skriv inn hvor du skal kjøre: ")
