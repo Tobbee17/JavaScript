a64eafb219064d82b4abe9d67979c77d9542b1d1
Tobias Engli Fladen <tobiae1805@osloskolen.no>
2021-10-26 10:02:25 +0200

    j

---
+++ oppgaveTall.py
@@ -0,0 +1,10 @@
+tall1 = float(input("Skriv inn et tall som er høyere enn 0: "))
+
+if tall1 < 1:
+    print("Tallet er mindre enn 1")
+
+elif tall1 > 10:
+    print("Tallet er større enn 10")
+
+elif tall1 >= 1 and tall1 <= 10:
+    print(tall1)
