a12611c99d23e309a97560ff429f52230831ccb4
Tobias Engli Fladen <tobiae1805@osloskolen.no>
2021-10-26 09:51:11 +0200

    ja

--- Tirsdag,26,10.py
+++ Tirsdag,26,10.py
@@ -1,5 +1,11 @@
 BensinTank = float(input("Skriv inn størrelsen på tanken: "))
 BensinForbruk = float(input("Skriv inn bensin forbruk pr mil: "))
 Pris = float(input("Skriv inn bensin prisen pr liter: "))
-Lengde = float(input("Skriv inn hvor du skal kjøre: "))
+Lengde = float(input("Skriv inn hvor langt du skal kjøre: "))
 
+AntallLiter = Lengde * BensinForbruk
+PrisForReise = Pris * AntallLiter
+DrivstoffIgjen = BensinTank - AntallLiter
+
+print(PrisForReise)
+print(DrivstoffIgjen)
