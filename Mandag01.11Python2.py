d0a406c27f6aa58bb55dac0d4c20c41f5c862aa1
Tobias Engli Fladen <tobiae1805@osloskolen.no>
2021-11-01 13:54:01 +0100

    Update Mandag01.11Python2.py

--- Mandag01.11Python2.py
+++ Mandag01.11Python2.py
@@ -2,12 +2,12 @@ balansen = float(input("Skriv inn balansen på kontoen din: "))
 
 penger = str(input("Skriv in Innskudd for å sette inn penger og Utak for å ta ut penger: "))
 
-if penger == "Innskudd":
+if "Inn" in penger:
     Innskudd=float(input("Skriv inn hvor mye penger du vil sette inn: "))
     InnskuddBalanse = Innskudd + balansen
     print("Ny balanse er: ", InnskuddBalanse, "Kr")
 
-elif penger == "Utak":
+elif "Ut" in penger:
     Utak=float(input("Hvor mye penger vil du ta ut? "))
     UtakBalanse = balansen - Utak
     print("Ny balanse er: ", UtakBalanse, "Kr")
