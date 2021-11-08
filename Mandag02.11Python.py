47f2a1f540b6a2a958ce8e237926b57e36f09538
Tobias Engli Fladen <tobiae1805@osloskolen.no>
2021-11-08 13:30:49 +0100

    while loop

--- Mandag02.11Python.py
+++ Mandag02.11Python.py
@@ -1,27 +1,32 @@
 Brukskonto = float(input("Hva er balansen på brukskontoen din?: "))
 Sparekonto = float(input("Hva er balansen på sparekontoen din?: "))
+inputStr=input("Trykk q for å avslutte eller trykk ENTER for å fortsette")
 
-Velgkonto=(input("Hvilken konto vil du velge? brukskonto eller sparekonto: "))
+while inputStr != "q":
 
-if "bruks" in Velgkonto:
-    Bruks=str(input("Du valgte Brukskonto. Vil du sette inn eller ta ut penger?: "))
-    if "inn" in Bruks:
-        penger=float(input("Hvor mye vil du sette inn?: "))
-        Brukskonto = Brukskonto + penger
+    Velgkonto=(input("Hvilken konto vil du velge? brukskonto eller sparekonto: "))
+
+    if "bruks" in Velgkonto:
+        Bruks=str(input("Du valgte Brukskonto. Vil du sette inn eller ta ut penger?: "))
+        if "inn" in Bruks:
+            penger=float(input("Hvor mye vil du sette inn?: "))
+            Brukskonto = Brukskonto + penger
         
-    elif "ut" in Bruks:
-        penger=float(input("Hvor mye penger vil du ta ut?: "))
-        Brukskonto = Brukskonto - penger
+        elif "ut" in Bruks:
+            penger=float(input("Hvor mye penger vil du ta ut?: "))
+            Brukskonto = Brukskonto - penger
 
-if "spare" in Velgkonto:
-    Spare=str(input("Du valgte sparekonto. Vil du inn eller ta ut penger?: "))
-    if "inn" in Spare:
-        penger=float(input("Hvor mye penger vil du sette inn?: "))
-        Sparekonto = Sparekonto + penger
+    if "spare" in Velgkonto:
+        Spare=str(input("Du valgte sparekonto. Vil du inn eller ta ut penger?: "))
+        if "inn" in Spare:
+            penger=float(input("Hvor mye penger vil du sette inn?: "))
+            Sparekonto = Sparekonto + penger
     
-    elif "ut" in Spare:
-        penger=float(input("Hvor mye penger vil du ta ut?"))
-        Sparekonto = Sparekonto - penger
-        
+        elif "ut" in Spare:
+            penger=float(input("Hvor mye penger vil du ta ut?"))
+            Sparekonto = Sparekonto - penger
+
+    inputStr=input("Trykk q for å avslutte eller trykk ENTER for å fortsette")
+
 print("Total brukskonto balanse er:" , Brukskonto, "Kr")
 print("Total sparekonto balanse er:" , Sparekonto, "Kr")
