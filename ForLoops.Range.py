6efade213c42619cb57053927c45f762776949f6
Tobias Engli Fladen <tobiae1805@osloskolen.no>
2021-11-23 13:52:34 +0100

    Create ForLoops.Range.py

---
+++ ForLoops.Range.py
@@ -0,0 +1,17 @@
+for i in range(5):
+    print(i)
+
+for j in range(1,5):
+    print(j)
+
+for a in range(1,10,2):
+    print(a)
+
+for b in range(10,1,-1):
+    print(b)
+
+for x in "banan":
+    print(x)
+
+for k in "kuben":
+    print(k,end=" ")
