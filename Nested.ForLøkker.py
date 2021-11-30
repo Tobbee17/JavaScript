592c19c133f7e2386cc822909605ce107b28dc05
Tobias Engli Fladen <tobiae1805@osloskolen.no>
2021-11-30 08:59:55 +0100

    Update Nested.ForLøkker.py

--- Nested.ForLøkker.py
+++ Nested.ForLøkker.py
@@ -4,11 +4,7 @@ for i in range(1,11): #Ytre for-løkke: bestemmer antall løkker
     print()
 
 from random import random, randint #metodene fra random-modul må importeres
-
-kast=randint(1,6) #gir ett tilfeldig heltall(integer) mellom 1 og 6 
-kast2=randint(1,6) #gir ett tilfeldig heltall(integer) mellom 1 og 6 
-for i in range(1,11): #Ytre løkke
-    print("Kast",i,"ble:",kast,"og",kast2) 
-    for j in range(2): #indre løkke 
-        print() # tom print-funksjon for å starte ny linje etter indre for-løkke.
-
+for i in range(1,11): #Ytre løkke 
+    kast=randint(1,6) #gir ett tilfeldig heltall(integer) mellom 1 og 6 
+    kast2=randint(1,6) #gir ett tilfeldig heltall(integer) mellom 1 og 6 
+    print("Kast ",i,"ble: ",kast,"og",kast2)
