b9d7e40662fdc6b114c8ca750d78015f4277a8fe
Tobbee17 <tobiae1805@osloskolen.no>
2022-01-07 10:33:19 +0100

    Oppgave e

--- OppgaveTilKlassen.py
+++ OppgaveTilKlassen.py
@@ -1,13 +1,7 @@
 # Importerer kalender 
-
 import calendar 
 
- 
-
-yy = 2021   # year 
-
-mm = 12   # month 
-
- 
+yy = 2021 # year 
+mm = 12 # month 
 
 print(calendar.month(yy, mm)) 
--- PrinterUtStoreBokstaver.py
+++ PrinterUtStoreBokstaver.py
@@ -1,8 +1,9 @@
 navn= str(input("Skriv inn en tekst: "))
 
-siffer = 0
+vokaler = 0
 
 for k in navn:
-    if k.isdigit():
-        siffer += 1
-print("Antall siffer er: ", siffer)
+    if (k == 'A' or k == 'E' or k == 'I' or k == 'O' or k == 'U' or k == 'Y' or k == 'Æ' or k == 'Ø' or k == 'Å' or k == 'a'
+       or k == 'e' or k == 'i' or k == 'o' or k == 'u' or k == 'y' or k == 'æ' or k == 'ø' or k == 'å'):
+        vokaler += 1
+print("Antall vokaler: ", vokaler)
