fd458c32a099980cfc9b81e78c702f47b48daadb
Tobbee17 <tobiae1805@osloskolen.no>
2022-01-10 09:44:55 +0100

    Update binærverdi.py

--- binærverdi.py
+++ binærverdi.py
@@ -2,30 +2,38 @@
 fireBit=input ("Skriv inn et 4-bits tall: ")
 
 # tiTall er variabelen som holder verdien til tallet i 10tallsystemet, og er satt til e
-tiTall=0
+tiTall = 0
 
 # variabelen Leng gir Lengden til firebitstallet
 leng=len(fireBit)
 
 # variabel counter er en tellevariabel
 # den brukes for å vite hvor Langt man har kommet i firebitstalLlet
-counter=0
+counter = 0
 
 # for-Løkken itererer gjennom hver "karakter" (tall) i input-string
 # siden k er en string, må vi gjøre om k til integer for å sjekke om den har verdi 1
 # det gjøres ved å caste: int(k)
 for k in fireBit:
     if counter == 0 and int(k) == 1:      # sjekker om første bit (fra venstre) er 1
-        tiTall=tiTall + 8                 # Legger til 8 hvis det stemmer
-    elif counter==-1 and k ==1:           # sjekker om andre bit er 1
-        tiTall = tiTall + 4               # Legger til 4 hvis det stemmer
-    elif counter == 2 and int (k) ==1:    # sjekker om tredje bit er 1
-        tiTall = tiTall + 2               # Legger til 2 hvis det stemmer
-    elif counter ==3 and int(k)==1:       # sjekker om fjerde bit er 1
-        tiTall=tiTall +1                  # Legger til 1 hvis det stemmer
-    else:                                 # denne gjør ingenting
+        tiTall = tiTall + 128
+    elif counter == 1 and int(k) == 1:          
+        tiTall = tiTall + 64 
+    elif counter == 2 and int(k) == 1:          
+        tiTall = tiTall + 32 
+    elif counter == 3 and int(k) == 1:         
+        tiTall = tiTall + 16                              
+    elif counter == 4 and int(k) == 1:         
+        tiTall = tiTall + 8             
+    elif counter == 5 and int(k) == 1:  
+        tiTall = tiTall + 4             
+    elif counter == 6 and int(k) == 1:    
+        tiTall = tiTall + 2
+    elif counter == 7 and int(k) == 1:          
+        tiTall = tiTall + 1   
+    else:         
         pass
-    counter = counter +1 # tellevariabelen øker med 1, for hver iterasjon
+    counter = counter + 1
 
 # printer feridg svar
 print ("de 4 bitene ", fireBit, "har verdi ", tiTall, "i 10tallsystemet" )
