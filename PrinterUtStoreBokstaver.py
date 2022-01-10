78d5c1ea9c632427d272afc006af41c7c35a9c8f
Tobbee17 <tobiae1805@osloskolen.no>
2022-01-10 09:02:02 +0100

    Update PrinterUtStoreBokstaver.py

--- PrinterUtStoreBokstaver.py
+++ PrinterUtStoreBokstaver.py
@@ -1,9 +1,8 @@
 navn= str(input("Skriv inn en tekst: "))
 
-vokaler = 0
+siffer = 0
 
 for k in navn:
-    if (k == 'A' or k == 'E' or k == 'I' or k == 'O' or k == 'U' or k == 'Y' or k == 'Æ' or k == 'Ø' or k == 'Å' or k == 'a'
-       or k == 'e' or k == 'i' or k == 'o' or k == 'u' or k == 'y' or k == 'æ' or k == 'ø' or k == 'å'):
-        vokaler += 1
-print("Antall vokaler: ", vokaler)
+    if k.isdigit():
+        siffer += 1
+print("Antall siffer er: ", siffer)
